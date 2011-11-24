63d4f024bc265cc341e52ca30c14c962d8669607
larry fang <larry.l.fang@gmail.com>
2011-11-24 21:25:41 +1100

    test

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,6 +1,7 @@
 class StatusController < ApplicationController
   def index
     @time = Time.now
+    @yaml = StatusHelper.array_methods
     @ps = `ps aux`
   end
 
--- app/helpers/status_helper.rb
+++ app/helpers/status_helper.rb
@@ -1,2 +1,10 @@
 module StatusHelper
+  def self.array_methods
+    config = ['st', 'sd', 3, 4]
+    result=[]
+    YAML.load_file(File.join(Rails.root, '/config/array.yml')).each do |key, value|
+      result << config.send(key, value)
+    end
+      result
+  end
 end
